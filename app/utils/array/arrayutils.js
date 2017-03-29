9e8b5ab8e186acb4ccc082b83b6e1d600f58a747
Chris Turns <chris.turns@jandltech.com>
2017-03-29 22:29:04 +0100

    ArrayUtils: Added code covering undefined checking.  Updated incorrect test.

--- app/utils/array/arrayutils.js
+++ app/utils/array/arrayutils.js
@@ -5,5 +5,20 @@ angular.module('primetables.utils.array', [
     .service("ArrayUtils", function () {
         this.setRecurringValue = function (array, startIndex, endIndex, increment, value) {
 
+            validateParameters();
+
+            function validateParameters() {
+                var CANNOT_BE_UNDEFINED = " cannot be undefined";
+
+                if(array === undefined) {
+                    throw "array" + CANNOT_BE_UNDEFINED;
+                } else if(startIndex === undefined) {
+                    throw "startIndex" + CANNOT_BE_UNDEFINED;
+                } else if(endIndex === undefined) {
+                    throw "endIndex" + CANNOT_BE_UNDEFINED;
+                } else if(increment === undefined) {
+                    throw "increment" + CANNOT_BE_UNDEFINED;
+                }
+            }
         };
     });
--- app/utils/array/arrayutils_test.js
+++ app/utils/array/arrayutils_test.js
@@ -27,7 +27,7 @@ describe('arrayutils_test.js', function () {
             it('should throw an error when endIndex is undefined', function() {
                 expect(function() {
                     ArrayUtils.setRecurringValue([], 1, undefined, 1, 1)
-                }).toThrow("startIndex cannot be undefined");
+                }).toThrow("endIndex cannot be undefined");
             });
 
             it('should throw an error when increment is undefined', function() {
