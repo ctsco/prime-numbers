76eb0a1ed3c224acc0f0ff947380360143c11e68
Chris Turns <chris.turns@jandltech.com>
2017-03-29 23:55:38 +0100

    PrimeUtils: Added implementation for isValuePrime function.  Possible performance improvements still to be made.

--- app/utils/primes/primeutils.js
+++ app/utils/primes/primeutils.js
@@ -4,7 +4,12 @@ angular.module('primetables.utils.primes', [
 ])
     .service("PrimeUtils", function () {
         this.isValuePrime = function (value) {
+            for(var loopIndex = value - 1; loopIndex >=2; loopIndex--) {
+                if(value%loopIndex === 0) {
+                    return false;
+                }
+            }
 
-
+            return true;
         };
     });
