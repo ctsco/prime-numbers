3ffe1292162588830b3b37f71b73bbcbb2668982
Chris Turns <chris.turns@jandltech.com>
2017-03-29 22:37:25 +0100

    ArrayUtils: Added tests covering start and end ranges.  Currently failing.

--- app/utils/array/arrayutils_test.js
+++ app/utils/array/arrayutils_test.js
@@ -36,7 +36,75 @@ describe('arrayutils_test.js', function () {
                 }).toThrow("increment cannot be undefined");
             });
 
+            it('should throw an error when endIndex is before startIndex', function() {
+                var START_INDEX = 2;
+                var END_INDEX = 1;
+
+                expect(function() {
+                    ArrayUtils.setRecurringValue([], START_INDEX, END_INDEX, 1, 1)
+                }).toThrow("endIndex cannot be before startIndex");
+            });
+
         });
+
+        describe("should update the array passed into the function", function() {
+
+            describe("between the specified start and end index", function() {
+
+                var VALUE = "MyValue";
+                var array;
+
+                beforeEach(function() {
+                    array = [];
+                });
+
+                it('when the start and end index are the same', function() {
+                    var START_INDEX = 0;
+                    var END_INDEX = 0;
+
+                    ArrayUtils.setRecurringValue(array, START_INDEX, END_INDEX, 1, VALUE);
+
+                    expect(array.length).toBe(1);
+                    expect(array[START_INDEX]).toBe(VALUE);
+                });
+
+                it('when the start and end index are not the same', function() {
+                    var START_INDEX = 0;
+                    var END_INDEX = 3;
+
+                    ArrayUtils.setRecurringValue(array, START_INDEX, END_INDEX, 1, VALUE);
+
+                    expect(array.length).toBe(4);
+
+                    expect(array[0]).toBe(VALUE);
+                    expect(array[1]).toBe(VALUE);
+                    expect(array[2]).toBe(VALUE);
+                    expect(array[3]).toBe(VALUE);
+                });
+
+                it('when the start is greater than 0', function() {
+                    var START_INDEX = 1000;
+                    var END_INDEX = 1001;
+
+                    ArrayUtils.setRecurringValue(array, START_INDEX, END_INDEX, 1, VALUE);
+
+                    expect(array.length).toBe(2);
+
+                    expect(array[1000]).toBe(VALUE);
+                    expect(array[1001]).toBe(VALUE);
+                });
+            });
+
+            describe("taking into consideration the increment", function() {
+
+            });
+
+            describe("with the value passed in", function() {
+
+            });
+
+        });
+
 
     });
 
