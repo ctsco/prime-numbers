0094a1f259052f46c5473e63b8ebdd4598cb1b68
Chris Turns <chris.turns@jandltech.com>
2017-03-30 16:10:12 +0100

    TableViewComponent: Added button to view.
    karma config: Explicitly loading jQuery before Angular to ensure we're not using jqLite.

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,48 @@
 //jshint strict: false
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    basePath: './app',
+        basePath: './app',
 
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      '**/app.js',
-      'tableview/**/*.js',
-      'utils/**/*.js',
-      '**/*.html'
-    ],
+        files: [
+            '../node_modules/jquery/dist/jquery.min.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            '**/app.js',
+            'tableview/**/*.js',
+            'utils/**/*.js',
+            '**/*.html'
+        ],
 
-    autoWatch: true,
+        autoWatch: true,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers: ['PhantomJS'],
+        browsers: ['PhantomJS'],
 
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter',
-      'karma-ng-html2js-preprocessor'
-    ],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-ng-html2js-preprocessor'
+        ],
 
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    },
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
 
-    preprocessors: {
-      '**/*.html': ['ng-html2js']
-    },
+        preprocessors: {
+            '**/*.html': ['ng-html2js']
+        },
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: '',
-      moduleName: 'templates'
-    }
+        ngHtml2JsPreprocessor: {
+            stripPrefix: '',
+            moduleName: 'templates'
+        }
 
-  });
+    });
 };
